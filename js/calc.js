ff578c6cb56ccff17884f7157bf650ced259c5b6
shrfrn <shrfrn@gmail.com>
2025-03-18 10:53:08 +0200

    Add click handler placeholders

---
+++ js/calc.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function onNumClick(elNum) {
+    console.log(elNum.innerText)
+}
+
+function onOperatorClick(elOperator) {
+    console.log(elOperator.innerText)
+}
+
+function onCalc() {
+    console.log('Calculating...')
+}
